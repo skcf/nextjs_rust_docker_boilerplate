e52ae985eb69673f4ba032da1d107982a16e7b0d
Souichi <sk.cf.msc@gmail.com>
2023-02-23 19:02:57 +0900

    add: logger

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,7 @@
+use actix_web::middleware::Logger;
+use env_logger;
 use actix_web::{get, web, App, HttpServer, Responder};
+
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -12,7 +15,14 @@ async fn hello(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(hello))
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    HttpServer::new(|| 
+            App::new()
+            .wrap(Logger::default())
+            .service(index)
+            .service(hello)
+        )
         .bind(("0.0.0.0", 8080))?
         .run()
         .await
